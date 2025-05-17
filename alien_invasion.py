2e34e53cb7d5421d0ba4c7b591416c94e460db65
sait <sait@example.com>
2025-05-18 00:08:47 +0300

    uzayli istilasi ilk kısım

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,8 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Uzayli Istilasi")
+        # Arka plan rengini ayarla.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Oyun için ana döngüyü başlat."""
@@ -22,6 +24,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Döngüden her geçişte ekranı yeniden çizdir.
+            self.screen.fill(self.bg_color)
             # En son çizilen ekranı görünür yap.
             pygame.display.flip()
 
