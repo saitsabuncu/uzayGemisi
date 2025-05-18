3d8ba82174a8c188dbfd84a192f4f8209eee798b
sait <sait@example.com>
2025-05-18 08:26:53 +0300

    ayarlar update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import sys, pygame
+from settings import Setting
 
 class AlienInvasion:
     """
@@ -12,7 +13,11 @@ class AlienInvasion:
         oyun kaynaklarını oluştur.
         """
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Setting()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,
+             self.settings.screen_height)
+        )
         pygame.display.set_caption("Uzayli Istilasi")
         # Arka plan rengini ayarla.
         self.bg_color = (230, 230, 230)
@@ -25,7 +30,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Döngüden her geçişte ekranı yeniden çizdir.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # En son çizilen ekranı görünür yap.
             pygame.display.flip()
 
