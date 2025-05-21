ab2859cc99baacc8ab9af0e24254cdf8b25c5c61
sait <sait@example.com>
2025-05-21 17:34:25 +0300

    bullet settings

--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ class Setting:
         self.bg_color = (230,230,230)
         # Gemi settings
         self.ship_speed = 1.5
+        # mermi settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
