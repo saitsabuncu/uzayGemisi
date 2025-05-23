8a89024453b91d93d682a728b134c86851ead4f5
sait <sait@example.com>
2025-05-23 22:23:30 +0300

    buton class create

---
+++ button.py
@@ -0,0 +1,14 @@
+import pygame.font
+
+class Button:
+
+    def __init__(self, ai_game, msg):
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        #düğmenin boyutları ve özellikleri
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255,255,255)
+        self.font = pygame.font.SysFont(None, 48)
+
--- game_stats.py
+++ game_stats.py
@@ -4,8 +4,9 @@ class GameStats:
         """istatistiklere ilk değer ata."""
         self.settings = ai_game.settings
         self.reset_stats()
-        # uzaylı istilasını aktif bir durumda başlat.
-        self.game_active = True
+
+        # oyunu aktif olmayan durumda başlat.
+        self.game_active = False
 
     def reset_stats(self):
         """oyun esnasında değişebilecek
